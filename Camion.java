cb820f9e042ad6f8cce7a5103a9901d5ff3ecc9d
kasereka12 <152752545+kasereka12@users.noreply.github.com>
2023-12-10 23:52:44 +0100

    Camion.java

--- Camion.java
+++ Camion.java
@@ -1,5 +1,16 @@
 package TP;
 
-public class Camion {
-
+public class Camion extends Vehicule{
+  private int ChargesMaximales;
+  public Camion(String numeroMatricule, String type, String marque, String couleur, int anneeFabrication,
+			Double poids, Double vitesse , int ChargesMaximales) {
+    super(numerpMatricule,type,marque,couleur,anneeFabrication,poids,vitesse);
+    this.ChargesMaximales = ChargesMaximales;      
+  }
+  @override
+  public String toString() {
+		return super.toString()+ "charges maximales :" + chargesMaximales;
+	}
+	
+  
 }
